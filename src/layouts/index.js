3ea9088861fae5efbb9d6553a8ad87bc18f84676
konstantinaxt <axtkonstantin@hotmail.de>
2017-10-14 10:33:21 +0200

    commit gatsby files

---
+++ src/layouts/index.js
@@ -0,0 +1,52 @@
+import React, {Component} from "react";
+import Link from "gatsby-link";
+
+const ListLink = props => (
+  <li style={{
+    display: `inline-block`,
+    marginRight: `1rem`
+  }}>
+    <Link to={props.to}>
+      {props.children}
+    </Link>
+  </li>
+);
+
+export default class Layout extends Component {
+
+  render() {
+
+    const {children} = this.props;
+
+    return (
+      <div style={{
+        margin: `0 auto`,
+        maxWidth: 1000,
+        padding: `1.25rem 1rem`
+      }}>
+        <header style={{marginBottom: `1.5rem`}}>
+          <Link to="/"
+                style={{
+                  textShadow: `none`,
+                  backgroundImage: `none`
+                }}>
+            <h3 style={{display: `inline`}}>MySweetSite</h3>
+          </Link>
+          <ul style={{
+            listStyle: `none`,
+            float: `right`
+          }}>
+            <ListLink to="/">Home</ListLink>
+            <ListLink to="/blog/">Blog</ListLink>
+            <ListLink to="/contact/">Contact</ListLink>
+            <ListLink to="/counter/counter">Counter</ListLink>
+            <ListLink to="/animation">Animation</ListLink>
+            <ListLink to="/my-files">MyFiles</ListLink>
+            <ListLink to="/slider">Slider</ListLink>
+          </ul>
+        </header>
+        {children()}
+      </div>
+    )
+  }
+}
---
+++ src/pages/index.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import Link from 'gatsby-link';
+
+export default class IndexPage extends Component {
+
+  render() {
+
+    return (
+      <div style={{background: "lightgrey"}}>
+        <h1>Hi people</h1>
+        <p>Welcome to your new Gatsby site.</p>
+        <p>Now go build something great.</p>
+        <Link to="/page-2/">Go to page 2</Link>
+      </div>
+    )
+  }
+}
